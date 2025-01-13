b15e382522331ad151f77defc795ded184f9110f
신은정 <ejshin.dev@gmail.com>
2025-01-13 17:40:07 +0900

    [level 1] Title: 하샤드 수, Time: 0.32 ms, Memory: 95 MB -BaekjoonHub

---
+++ 프로그래머스/1/12947. 하샤드 수/하샤드 수.java
@@ -0,0 +1,16 @@
+class Solution {
+    public boolean solution(int x) {
+        int sum = 0;
+        String num[] = Integer.toString(x).split("");
+        
+        for(int i = 0; i < num.length; i++) {
+            sum += Integer.parseInt(num[i]);
+        }
+        
+        if(x % sum == 0) {
+            return true;
+        } else {
+            return false;
+        }
+    }
+}
