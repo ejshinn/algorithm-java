d39ef12e149b98f8b58084dc5e117283463cb8f2
신은정 <ejshin.dev@gmail.com>
2025-02-06 03:19:25 +0900

    [level 1] Title: 2016년, Time: 0.02 ms, Memory: 80.5 MB -BaekjoonHub

---
+++ 프로그래머스/1/12901. 2016년/2016년.java
@@ -0,0 +1,15 @@
+class Solution {
+    public String solution(int a, int b) {
+        int total = 0;
+        String[] days = {"SUN", "MON", "TUE", "WED", "THU", "FRI", "SAT"};
+        int[] months = {0, 31, 29, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31};
+        
+        for(int i = 0; i < a; i++) {
+            total += months[i];
+        }
+        
+        total += b;
+
+        return days[(total + 4) % 7];
+    }
+}
